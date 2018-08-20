b64ac3182277eb3e6f7ad26b77c7f34cb544d1a0
FedericoCesari <federicojorgecesari@gmail.com>
2018-08-20 19:26:22 -0300

    Añadiendo el archivo Prueba

---
+++ adivina_el_numero.py
@@ -0,0 +1,11 @@
+
+
+number_to_guess = 2
+
+user_number = int(input("Deci un numero, a ver si adivinas: "))
+
+if number_to_guess == user_number:
+    print("Ganaste")
+else:
+    print("No le acertaste, pero tenes otra oportunidad")
+
